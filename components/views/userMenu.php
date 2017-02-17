240db2a0399530de53318713c56bb4281455026d
account931 <account931@ukr.net>
2017-02-17 13:17:37 +0200

    Add files via upload

---
+++ components/views/userMenu.php
@@ -0,0 +1,15 @@
+<!--  right  side bar  menu, initiated  in  view-layouts-column2 -->
+<ul>
+	<li><?php echo CHtml::link('LightSpeed','http://ls.igniteso.com/', array('target'=>'_blank'));   ?></li>
+        <li><?php echo CHtml::link('Zimbra Mail','https://mail.igniteoutsourcing.com/#1', array('target'=>'_blank'));   ?></li>
+	<!--<li><?php echo CHtml::link('Waze Time','http://example2.esy.es/myWazeTime/index.php?user=ign_dmitriy', array('target'=>'_blank'));   ?></li>-->
+        <li><?php echo CHtml::link('Waze ','https://www.waze.com', array('target'=>'_blank'));   ?></li>
+        <li><?php echo CHtml::link('Venues  Rules ','https://docs.google.com/spreadsheets/d/1hryT1PLETjRPqydxnx8ycUnaPLYT3cJqCCzJHgkUXks/edit?ts=579f172d#gid=0', array('target'=>'_blank'));   ?></li>
+
+	
+</ul>
+
+
+
+
+
